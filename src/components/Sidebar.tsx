3425fd5d054765281f59c9984b1a194d4d695c3d
raphasm <raphasm.dev@hotmail.com>
2023-08-15 11:37:44 -0300

    initial commit

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,29 @@
+import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar';
+
+import styles from './Sidebar.module.css'
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img 
+        className={styles.cover} 
+        src="https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/raphasm.png" />
+        <strong>Raphael Margoni</strong>
+        <span>Web Developer</span>
+      </div>
+
+    <footer>
+      <a href="#">
+        <PencilLine size={20} />
+        Editar seu perfil
+      </a>
+    </footer>
+
+    </aside>
+  );
+}
